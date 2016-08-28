bff4dd2cb4f421933a8f46596da2213fd6eedba2
tal <tal32123@gmail.com>
2016-08-28 04:24:17 -0500

    Attempt to implement Retrofit Library

--- app/src/main/java/com/sam_chordas/android/stockhawk/ui/MyChartActivity.java
+++ app/src/main/java/com/sam_chordas/android/stockhawk/ui/MyChartActivity.java
@@ -43,20 +43,6 @@ public class MyChartActivity extends Activity {
         lineChartView.setLabelsColor(0xffffffff);
         lineChartView.show();
     }
-
-    //start querying data from API
-    // Trailing slash is needed
-
-//    public interface YQLService {
-//        @GET("users/{user}/repos")
-//        Call<List<Repo>> listRepos(@Path("user") String user);
-//    }
-//    public static final String BASE_URL = "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.historicaldata%20where%20symbol%20%3D%20%22YHOO%22%20and%20startDate%20%3D%20%222009-09-11%22%20and%20endDate%20%3D%20%222010-03-10%22&format=json&diagnostics=true&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback=";
-//    Retrofit retrofit = new Retrofit.Builder()
-//            .baseUrl(BASE_URL)
-//            .addConverterFactory(GsonConverterFactory.create())
-//            .build();
-//
-//    YQLService service = retrofit.create(YQLService.class);
+    
 
 }
